e653c9b93b7c13947391c3aa390e18dd622f4286
Gayheart2506 <107317418+Gayheart2506@users.noreply.github.com>
2023-10-03 18:19:28 +0000

    Made some changes and update, to exit from game you type exit to quit the program

--- main.py
+++ main.py
@@ -17,6 +17,15 @@ while len(guessed_states) < 50:
     
     state_answer = screen.textinput(title=f"Guessed {len(guessed_states)}/50 correct states", prompt="What's another state in the USA:").title()
 
+    if state_answer == "Exit":
+        unknown_states = []
+        for states in states_column:
+            if states not in guessed_states:
+                unknown_states.append(states)
+        new_data = pandas.DataFrame(unknown_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+
     if state_answer in states_column:
         guessed_states.append(state_answer)
         t = turtle.Turtle()
@@ -26,5 +35,3 @@ while len(guessed_states) < 50:
         t.goto(int(state_row.x), int(state_row.y))
         t.write(state_row.state.item())
 
-
-screen.exitonclick()
